abe83525da83a35ec2151b3ed390bfd9e911f1b1
Thomas Klausner <domm@plix.at>
2014-11-30 00:14:24 +0100

    added inital version

---
+++ lib/File/FindLocalLib.pm
@@ -0,0 +1,134 @@
+package File::FindLocalLib;
+use strict;
+use warnings;
+use 5.010;
+
+use FindBin qw();
+use Carp qw(carp);
+use File::Spec::Functions qw(catdir splitdir);
+use local::lib qw();
+
+my $ROOT;
+
+sub import {
+    my $class = shift;
+    my @libdirs;
+    my @locallibs;
+    foreach my $d (@_) {
+        if ( $d =~ /^local::lib=(\w+)/ ) {
+            push( @libdirs,   $1 );
+            push( @locallibs, $1 );
+        }
+        else {
+            push( @libdirs, $d );
+        }
+    }
+
+    my @searchdirs = splitdir("$FindBin::Bin");
+
+SEARCH: while (@searchdirs) {
+        foreach my $dir (@libdirs) {
+            unless ( -d catdir( @searchdirs, $dir ) ) {
+                pop(@searchdirs);
+                next SEARCH;
+            }
+        }
+        $ROOT = catdir(@searchdirs);
+        last SEARCH;
+    }
+
+    if ($ROOT) {
+        local::lib->import( map { catdir( $ROOT, $_ ) } @locallibs ) if @locallibs;
+        lib->import( map        { catdir( $ROOT, $_ ) } @libdirs ) if @libdirs;
+    }
+    else {
+        carp "Could not find root dir containing " . join( ', ', @libdirs );
+    }
+}
+
+sub load_extra {
+    my $class  = shift;
+    my @extras = @_;
+
+    my @parts = splitdir($ROOT);
+    pop(@parts);
+    my $parent = catdir(@parts);
+
+    foreach my $d (@extras) {
+        my $extra = catdir( $parent, $d, 'lib' );
+        if ( -d $extra ) {
+            push( @INC, $extra );
+        }
+        else {
+            carp "Cannot load_extra $d, directory $extra does not exist";
+        }
+    }
+}
+
+42;
+
+__END__
+
+=pod
+
+=head1 SYNOPSIS
+
+  # your_project/bin/somewhere/deep/down/script.pl
+  use strict;
+  use warnings;
+  # look up from the file's location until we find a directory containing a directory
+  # named 'lib'. Add this dir to @INC
+  use File::FindLocalLib qw(lib);
+
+  # look up until we find a dir that contains both 'lib' and 'foo', add both to @INC
+  use File::FindLocalLib qw(lib foo);
+
+  # look up until we find 'lib' and 'local'. Add 'lib' to @INC, load 'local' via local::lib
+  use File::FindLocalLib qw(lib local::lib=local);
+
+  # based on the dir we found earlier, go up one dir and try to add
+  # 'Your-OtherModule/lib' and 'Dark-PAN/lib' to @INC
+  File::FindLocalLib->load_extra(Your-OtherModule Dark-PAN);
+
+
+=head1 DESCRIPTION
+
+I'm usually using a setup like this:
+
+  .
+  ├── AProject
+  │   ├── bin
+  │   │   ├── db
+  │   │   │   └── init.pl
+  │   │   ├── onetime
+  │   │   │   ├── fixup
+  │   │   │   │   └── RT666_fix_up_fubared_data.pl
+  │   │   │   └── import_data.pl
+  │   │   └── web.psgi
+  │   ├── lib
+  │   └── local
+  ├── MyHelperStuff
+  │   └── lib
+  └── CoolLib-NotYetOnCPAN
+      └── lib
+
+There is C<AProject>, which is the actual code I'm working on. There is also probably C<BProject>, e.g. another microservice for the same customer. C<A-Project> has it's own code in C<lib> and it's CPAN dependencies in C<local> (managed via C<Carton> and used via C<local::lib>). There are a bunch of scripts / "binaries" in C<bin>, in a lot of different directories of varying depth.
+
+I have some generic helper code I use in several projects in C<MyHelperStuff/lib>. It will never go to CPAN. I have some other code in C<CoolLib-NotYetOnCPAN/lib> (but it might end up on CPAN if I ever get to clean it up...)
+
+C<File::FindLocalLib> makes it easy to add all these paths to C<@INC> so I can use the code.
+
+In each script, I just have to say:
+
+  use File::FindLocalLib qw(lib local::lib=local);
+
+C<lib> is added to the beginning of <@INC>, and C<local> is loaded via C<local::lib>, without me having to know how deep in C<bin> the current script is located.
+
+I can also add
+
+  File::FindLocalLib->load_extra(qw(MyHelperStuff CoolLib-NotYetOnCPAN));
+
+to get my other code pushed to C<@INC>. (Though currently I put this line, and some other setup code like initialising C<Log::Any> into C<AProject::Run>, and just C<use AProject::Run;>)
+
+
+
